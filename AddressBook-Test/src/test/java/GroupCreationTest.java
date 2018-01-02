3efc5d5c8990a349b0bfd27b345ea47df97ab448
Anton1981 <doktorkley81@mail.ru>
2018-01-02 05:27:16 +0200

    AddressBook-TestGroup
    CreateGroup

---
+++ AddressBook-Test/src/test/java/GroupCreationTest.java
@@ -0,0 +1,86 @@
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class GroupCreationTest {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    
+    @Test
+    public void GroupCreationTest() {
+        openAddressBook();
+        login();
+        goToGroupsPage();
+        initGroupCreation();
+        fillGroupForm("name", "header", "footer");
+        submitGroupCreation();
+        returnToGroupsPage();
+    }
+
+    public void returnToGroupsPage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    public void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(String name, String header, String footer) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(name);
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(header);
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(footer);
+    }
+
+    public void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    public void goToGroupsPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    public void login() {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.xpath("//*[@type='submit']")).click();
+    }
+
+    public void openAddressBook() {
+        wd.get("http://localhost/addressbook/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
