c24b04e83cae1a4fbb06f5ddb6d591cc31e5a505
Anton1981 <doktorkley81@mail.ru>
2018-01-03 21:16:16 +0200

    GroupDeletionCreationAndTestBase

--- AddressBook-Test/src/test/java/GroupCreationTest.java
+++ AddressBook-Test/src/test/java/GroupCreationTest.java
@@ -8,19 +8,10 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
-public class GroupCreationTest {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-    
+public class GroupCreationTest extends TestBase{
+
     @Test
     public void GroupCreationTest() {
-        openAddressBook();
-        login();
         goToGroupsPage();
         initGroupCreation();
         fillGroupForm("name", "header", "footer");
@@ -28,59 +19,4 @@ public class GroupCreationTest {
         returnToGroupsPage();
     }
 
-    public void returnToGroupsPage() {
-        wd.findElement(By.linkText("group page")).click();
-    }
-
-    public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
-    }
-
-    public void fillGroupForm(String name, String header, String footer) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(name);
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(header);
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(footer);
-    }
-
-    public void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
-    }
-
-    public void goToGroupsPage() {
-        wd.findElement(By.linkText("groups")).click();
-    }
-
-    public void login() {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//*[@type='submit']")).click();
-    }
-
-    public void openAddressBook() {
-        wd.get("http://localhost/addressbook/");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 }
---
+++ AddressBook-Test/src/test/java/GroupsDeletionTest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class GroupsDeletionTest extends TestBase{
+
+    @Test
+    public void GroupsDeletionTest() {
+        goToGroupsPage();
+        selectGroup();
+        initGroupsDeletion();
+        returnToGroupsPage();
+    }
+}
