fb007c4e49e01b849b322c3f3dd72bbfc610aa69
Anton1981 <doktorkley81@mail.ru>
2018-02-10 22:58:00 +0200

    TestNgTest

---
+++ TestNg-test/src/test/java/AmetodsTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class AmetodsTest {
+
+    @Test(groups = {"positive", "broken"})
+    public void test1A() {
+        System.out.println("broken testA");
+    }
+
+    @Test(groups = "positive")
+    public void test2A() {
+        System.out.println("positive testA");
+    }
+
+
+    @Test(groups = "negative")
+    public void test3A() {
+        System.out.println("negative testA");
+    }
+}
---
+++ TestNg-test/src/test/java/MetodsTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+public class MetodsTest {
+
+    @Test(priority = 1)
+    public void test2(){
+        System.out.println("test1");
+    }
+
+    @Test(priority = 2)
+    public void test1(){
+        System.out.println("test2");
+    }
+
+
+
+    public void notATest(){
+        System.out.println("notATest");
+    }
+}
